10499054387992fb0d555eed8d15093514bc6b62
lilKriT <lilKriTwebDev@gmail.com>
2026-02-08 01:41:32 +0100

    Added new files.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <p>You’ve used 815 GB of your storage</p>
+
+      <p>185 GB Left</p>
+
+      <p>0 GB</p>
+      <p>1000 GB</p>
+    </div>
+  );
 }
