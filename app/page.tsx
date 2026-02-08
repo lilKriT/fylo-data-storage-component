2453f6aefa6c819dcd42b20969f8c16777eb2b21
lilKriT <lilKriTwebDev@gmail.com>
2026-02-08 02:55:44 +0100

    Lots of styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
+const iconStyle =
+  "flex justify-center items-center w-12 aspect-square bg-blue-950 rounded-xl";
+
 export default function Home() {
   return (
     <section className="min-h-dvh flex justify-center items-center">
       <div className="flex gap-8">
         {/* Left box */}
-        <div className="min-w-[22rem] flex flex-col gap-8 bg-blue-850 p-10 rounded-xl rounded-tr-[6rem]">
+        <div className="min-w-88 flex flex-col gap-8 bg-blue-850 p-10 rounded-xl rounded-tr-[6rem]">
           <Image
             src={"/logo.svg"}
             alt="Fylo Logo"
@@ -14,7 +17,7 @@ export default function Home() {
             className=""
           />
           <div className="flex gap-4">
-            <div className="flex justify-center items-center w-12 aspect-square bg-blue-950 rounded-xl">
+            <div className={iconStyle}>
               <Image
                 src={"/icon-document.svg"}
                 alt="Document"
@@ -23,7 +26,7 @@ export default function Home() {
                 className=""
               />
             </div>
-            <div className="flex justify-center items-center w-12 aspect-square bg-blue-950 rounded-xl">
+            <div className={iconStyle}>
               <Image
                 src={"/icon-folder.svg"}
                 alt="Folder"
@@ -31,7 +34,7 @@ export default function Home() {
                 height={20}
               />
             </div>
-            <div className="flex justify-center items-center w-12 aspect-square bg-blue-950 rounded-xl">
+            <div className={iconStyle}>
               <Image
                 src={"/icon-upload.svg"}
                 alt="Upload"
@@ -41,13 +44,25 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div>
-          <p>You’ve used 815 GB of your storage</p>
 
+        {/* Right box */}
+        <div className="flex flex-col justify-between items-start bg-blue-850 p-10 rounded-xl">
+          <p>
+            You’ve used{" "}
+            <strong className="font-semibold text-blue-200">815 GB</strong> of
+            your storage
+          </p>
+
+          {/* Custom made progress bar */}
+          <div className="w-full h-6 rounded-full bg-blue-950 p-1">
+            <div className="w-[81.5%] h-full rounded-full bg-linear-to-r from-grad-orange to-grad-red"></div>
+          </div>
+
+          <div className="flex w-full justify-between">
+            <p>0 GB</p>
+            <p>1000 GB</p>
+          </div>
           <p>185 GB Left</p>
-
-          <p>0 GB</p>
-          <p>1000 GB</p>
         </div>
       </div>
     </section>
